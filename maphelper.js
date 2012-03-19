a108f3cfdf4ec8b0ff7ba1b661e63972d7bd0c96
Nathan Rosenbloom <nrosenbloom123@gmail.com>
2012-03-18 23:07:33 -0400

    changed to use parse, also you can add points which will be saved on the server

--- maphelper.js
+++ maphelper.js
@@ -1,96 +1,119 @@
+var applicationid = "oEhx7MD4NJ9dmwOTJVYIsDtSPRwxYYlXSwm13dI3";
+var apikey = "jd6bIkiEcWb89gHyQUuMYhst6d6hkWkcB3ySWzUv";
+var startlat = 38.971667;
+var startlong = -95.235278;
+var ajax;
 var map;
 var marker;
-var ajax;
 var publicmarkers = [];
 
-function initialize() {
-  ajax = new XMLHttpRequest();
-  ajax.onreadystatechange = function()
-  {
-    if(ajax.readyState == 4)
-    {
-      try{
-        var jason = eval("(" + ajax.responseText + ")");
-      }catch(e){
-        alert("responseText " + ajax.responseText);
-      }
-      makeMap(jason.lat, jason.long);
-      addMarkers();
-    }
-  }
-  ajax.open("POST", "gethome.php",true);
-  ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-  ajax.send("guid=" +1);}
+function init(){
+	makeMap();
+	getMarkers();
+}
 
-function addMarkers(){
-    ajax = getXmlHttp();
-    ajax.onreadystatechange =function(){
-    if(ajax.readyState ==4){
-      try{
-      var jason = eval("(" + ajax.responseText + ")");
-      }catch(e){
-        alert(ajax.responseText);
-      }
-      for(var i = 0; i < jason.markers.length; i++){
-        makeMarker(jason.markers[i].lat, jason.markers[i].long);
-      }
-    }
+function makeMap(){
+  var coord = new google.maps.LatLng(startlat, startlong);
+  var mapOptions = {
+    center: coord,
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
   };
-  ajax.open("POST", "getmarkers.php",true);
-  ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-  ajax.send("guid=" + 1);
-}
-
-function uploadTempMarker(){
-  if(marker){
-    ajax = getXmlHttp()
-    ajax.onreadystatechange = function(){
-      if(ajax.readyState==4)
-        alert(ajax.responseText);
-        addMarkers();
-    };
-    var markerpos = marker.getPosition();
-
-    ajax.open("POST", "addmarker.php", true);
-    ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    ajax.send("guid="+1+"&lat="+markerpos.lat()  + "&long=" + markerpos.long());
-  }
-}
-
-
-function makeMarker(lat, long){
-  newmarker = new google.maps.Marker({
-      position : new google.maps.LatLng(lat,long),
-      map : map,
+  map =  new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    google.maps.event.addListener(map, 'click', function(event) {
+    addTempMarker(event.latLng);
   });
-  publicmarkers.push(newmarker);}
+}
+
+
+
+function getMarkers(){
+	ajax = xmlhttp(function(){
+		if(ajax.readyState == 4){
+			var markers = eval( "(" + ajax.responseText + ")");
+			for(var i = 0; i <= markers.results.length; i++)
+			{
+				publicmarkers.push(new google.maps.Marker({position:new google.maps.LatLng(markers.results[i].location.latitude, markers.results[i].location.longitude),map:map}));
+			}
+		}
+	});
+	ajax.open("GET", "https://api.parse.com/1/classes/markers",true);
+	ajax.setRequestHeader("X-Parse-Application-Id", applicationid);
+	ajax.setRequestHeader("X-Parse-REST-API-Key", apikey);
+	ajax.setRequestHeader("Content-Type","application/json");
+	ajax.send();
+}
 
 function addTempMarker(location) {
   if(marker){
       marker.setMap(null);
   }
+  infowindow = new google.maps.InfoWindow({
+  	content:"<div id='tempwindow'> <INPUT TYPE='button' NAME='addmarker' Value='Add to Map' onClick='addMarker("+location.lat()+","+location.lng()+")'>"
+  });
   marker = new google.maps.Marker({
     position:location,
     map: map,
   });
-  marker.set
+  infowindow.open(map, marker);
 }
 
-function makeMap(lat, long){
-  var coord = new google.maps.LatLng(lat, long);
-  var mapOptions = {
-    center: coord,
-    zoom: 20,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map =  new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    google.maps.event.addListener(map, 'click', function(event) {
-    addTempMarker(event.latLng);
-  });}
-function getXmlHttp(){  var xmlhttprequest;
-  try{
-    xmlhttprequest = new XMLHttpRequest(); 
-  }catch(e){
-    alert("ERROR GETTING XMLHTTPREQUEST!");
-  }
-  return xmlhttprequest;}
+function addMarker(lat, long){
+		var ajax  = xmlhttp(function(){
+			if(ajax.readyState == 4){
+				document.getElementById("test").innerHTML = ajax.responseText;
+				getMarkers();
+			}
+			
+		});
+		var sendMarker = '{"location": {"__type":"GeoPoint", "latitude":'+lat+', "longitude":'+long+'}}';
+
+		ajax.open("POST", "https://api.parse.com/1/classes/markers",true);
+		ajax.setRequestHeader("X-Parse-Application-Id", applicationid);
+		ajax.setRequestHeader("X-Parse-REST-API-Key", apikey);
+		ajax.setRequestHeader("Content-Type","application/json");
+		ajax.send(sendMarker);
+	}
+
+function xmlhttp(func){
+	try{
+		temp = new XMLHttpRequest();
+	}catch(e){
+		alert("ERROR SUBMITTING AJAX REQUEST");
+	}
+	temp.onreadystatechange = func;
+	return temp;
+}
+
+function del(ID){
+			var newthing  = xmlhttp(function(){
+		
+		});
+
+		newthing.open("DELETE", "https://api.parse.com/1/classes/markers/"+ID,true);
+		newthing.setRequestHeader("X-Parse-Application-Id", applicationid);
+		newthing.setRequestHeader("X-Parse-REST-API-Key", apikey);
+		newthing.send();
+}
+
+function deletealot(){
+	ajax = xmlhttp(function(){
+		if(ajax.readyState == 4){
+			var markers = eval( "(" + ajax.responseText + ")");
+							document.getElementById("test").innerHTML += ajax.responseText;
+
+			for(var i = 0; i < markers.results.length; i++)
+			{
+
+				del(markers.results[i].location.objectId);
+			}
+			init();
+		}
+	});
+	ajax.open("GET", "https://api.parse.com/1/classes/markers",true);
+	ajax.setRequestHeader("X-Parse-Application-Id", applicationid);
+	ajax.setRequestHeader("X-Parse-REST-API-Key", apikey);
+	ajax.setRequestHeader("Content-Type","application/json");
+	ajax.send();
+	
+}
