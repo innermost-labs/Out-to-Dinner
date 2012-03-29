0aa2cd0d646bdf773a7cc2a2a64a0b4be8c1362b
Nathan Rosenbloom <nrosenbloom123@gmail.com>
2012-03-29 01:38:57 -0400

    added list subscribing code. don't know if it works because the rest if broken, also no one knows how to get the unique URLs

---
+++ inc/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+$api_key = 'ef0d5320f8d18d93eb46ded0787e4164-us4';
+
+// A List Id to run examples against. use lists() to view all
+// Also, login to MC account, go to List, then List Tools, and look for the List ID entry
+$listId = '460075e333';
+
+// A Campaign Id to run examples against. use campaigns() to view all
+$campaignId = 'YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method';
+
+//some email addresses used in the examples:
+$boss_man_email = 'INVALID@example.com';
+
+//just used in xml-rpc examples
+$apiUrl = 'http://api.mailchimp.com/1.3/';
+
+?>
---
+++ src/mailchimpsubscribe.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once '../inc/MCAPI.class.php'; //api class
+require_once '../inc/config.inc.php'; //apikey saved as $api_key
+
+$api = new MCAPI($api_key);
+//$api->useSecure(true);
+$merge_vars = array('FNAME'=>$_POST['fname'], 'LNAME'=>$_POST['lname'], 'MERGE3'=>$_POST['UURL']);
+$email_addr = $_POST['email'];
+
+$result =  $api->listSubscribe($listId,$email_addr, $merge_vars);
+
+//echo(" | " + $result + " |");
+
+/**if($api->errorCode){
+
+	echo "\n\tCode=".$api->errorCode;
+	echo "\n\tMsg=".$api->errorMessage."\n";
+return $result;
+} **/
+
+?>
+
+
+
