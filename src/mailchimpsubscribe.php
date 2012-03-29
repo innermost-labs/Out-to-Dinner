1aca5174626e7b38e192afe3f2ad0741deb7dd03
Nathan Rosenbloom <nrosenbloom123@gmail.com>
2012-03-29 02:17:29 -0400

    added cookie.js file, bug fix

--- src/mailchimpsubscribe.php
+++ src/mailchimpsubscribe.php
@@ -5,19 +5,21 @@ require_once '../inc/config.inc.php'; //apikey saved as $api_key
 
 $api = new MCAPI($api_key);
 //$api->useSecure(true);
-$merge_vars = array('FNAME'=>$_POST['fname'], 'LNAME'=>$_POST['lname'], 'MERGE3'=>$_POST['UURL']);
+//$merge_vars = array('FNAME'=>$_POST['fname'], 'LNAME'=>$_POST['lname'], 'MERGE3'=>$_POST['UURL']);
+$merge_vars = array('MERGE3'=>$_POST['MERGE3']);
 $email_addr = $_POST['email'];
 
 $result =  $api->listSubscribe($listId,$email_addr, $merge_vars);
-
-//echo(" | " + $result + " |");
-
-/**if($api->errorCode){
+/**
+echo(" | " + $result + " |");
+if($api->errorCode){
 
 	echo "\n\tCode=".$api->errorCode;
 	echo "\n\tMsg=".$api->errorMessage."\n";
+}
+**/
 return $result;
-} **/
+
 
 ?>
 
