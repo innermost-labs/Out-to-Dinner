169cec50f3383364e192cbf8bf4209508f6fa56f
Yale Spector <yspector@mdsol.com>
2012-03-29 23:52:12 -0400

    mailchimp win

--- src/mailchimpsubscribe.php
+++ src/mailchimpsubscribe.php
@@ -1,28 +1,13 @@
 <?php
-
 require_once '../inc/MCAPI.class.php'; //api class
 require_once '../inc/config.inc.php'; //apikey saved as $api_key
 
 $api = new MCAPI($api_key);
-//$api->useSecure(true);
-//$merge_vars = array('FNAME'=>$_POST['fname'], 'LNAME'=>$_POST['lname'], 'MERGE3'=>$_POST['UURL']);
-$merge_vars = array('MERGE3'=>$_POST['MERGE3']);
+$merge_vars = array('FNAME'=>$_POST['first_name'], 'MERGE3'=>$_POST['url']);
 $email_addr = $_POST['email'];
 
 $result =  $api->listSubscribe($listId,$email_addr, $merge_vars);
 
 
 echo(" | " + $result + " |");
-/**
-if($api->errorCode){
-
-	echo "\n\tCode=".$api->errorCode;
-	echo "\n\tMsg=".$api->errorMessage."\n";
-}
-
-return $result;
-**/
 ?>
-
-
-
