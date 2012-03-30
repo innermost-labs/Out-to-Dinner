fa41e83bd8856ce666865692915561282a013c7c
Chris Stathis <cstathis@gmail.com>
2012-03-30 00:05:45 -0400

    fixed volunteerUserAs

--- src/apihelper.js
+++ src/apihelper.js
@@ -106,7 +106,7 @@ var volunteerUserAs = function(type, form) {
     dataIn[type] = true;
   }
 
-  parseApiCall("PUT", myObjectId, dataIn, volunteerCallback,
+  parseApiCall("PUT", $.cookie("otd_objectId"), dataIn, volunteerCallback,
   volunteerErrorCallback, $.cookie("otd_sessionToken"));
 };
 
