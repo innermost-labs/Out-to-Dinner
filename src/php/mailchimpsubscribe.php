986c91bb66295a3d37fae1323090d180ea7a462f
Sahar Massachi <sayhar@gmail.com>
2012-04-02 22:56:38 -0400

    testing an edit to mailchimpsubscribe to handle zip codes as well.

--- src/php/mailchimpsubscribe.php
+++ src/php/mailchimpsubscribe.php
@@ -3,7 +3,7 @@ require_once '../inc/MCAPI.class.php'; //api class
 require_once '../inc/config.inc.php'; //apikey saved as $api_key
 
 $api = new MCAPI($api_key);
-$merge_vars = array('FNAME'=>$_POST['first_name'], 'MERGE3'=>$_POST['url']);
+$merge_vars = array('FNAME'=>$_POST['first_name'], 'MERGE3'=>$_POST['url'], 'MERGE4'=>$_POST['zip_code'] );
 $email_addr = $_POST['email'];
 
 $result =  $api->listSubscribe($listId,$email_addr, $merge_vars);
